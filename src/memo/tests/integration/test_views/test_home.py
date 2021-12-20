5d78e382a55c3014097a2aba3436c9c2b7fc2008
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2021-12-20 17:45:14 +0300

    Refactor home-page test

--- src/memo/tests/integration/test_views/test_home.py
+++ src/memo/tests/integration/test_views/test_home.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 
 class HomePageTest(TestCase):
-    def test_response_from_home_page_view(self):
+    def test_get_home_page_view(self):
         resp = self.client.get(reverse('memo:home'))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'home.html', 'base.html')
