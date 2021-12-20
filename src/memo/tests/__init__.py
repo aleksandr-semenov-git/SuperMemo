2583123b8291cb6f63f4b2c50b4005bd7bdf9fa8
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2021-12-20 13:54:20 +0300

    Replace and rename all old tests

--- src/memo/tests/__init__.py
+++ src/memo/tests/__init__.py
@@ -1,7 +1,1 @@
-import os
 
-from .old_test_models import MemoTestCases
-from .old_test_forms import AddGoalFormTest, PersonalDataEditFormTest
-from .old_test_views import ProfilePageTest
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'src.settings'
