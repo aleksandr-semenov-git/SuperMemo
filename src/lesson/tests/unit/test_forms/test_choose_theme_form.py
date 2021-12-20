d8d7af089574620c4fd4ffda0bcf317f93af799c
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2021-12-20 15:02:27 +0300

    Add test to choose-theme-form

--- src/lesson/tests/unit/test_forms/test_choose_theme_form.py
+++ src/lesson/tests/unit/test_forms/test_choose_theme_form.py
@@ -7,11 +7,19 @@ class ThemeFormsTest(SimpleTestCase):
     FILE_PATH = 'lesson.forms'
 
     @patch(f'{FILE_PATH}.Theme.objects')
-    def test_init_section_id_exists(self, patch_theme):
+    @patch(f'{FILE_PATH}.forms.Form.__init__')
+    def test_init_goal_id_exists(self, patch_init, patch_theme):
+        test_section_id = 1
         mock_theme = MagicMock()
         mock_theme.all.return_value = mock_theme
         patch_theme.filter.return_value = mock_theme
+        args = ()
+        kwargs = {'section_id': test_section_id}
 
-        form = ChooseThemeForm(section_id=1)
+        ChooseThemeForm.fields = {'name': MagicMock()}
+        form = ChooseThemeForm(*args, **kwargs)
+
+        patch_init.assert_called_once_with(*args, **{})
+
         self.assertEqual(form.fields['name'].queryset, mock_theme)
-        patch_theme.filter.assert_called_once()
+        patch_theme.filter.assert_called_once_with(section__id=test_section_id)
