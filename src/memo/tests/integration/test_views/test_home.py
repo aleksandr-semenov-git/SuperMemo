9cfd9884387600d2c967a46a92ab90698f413d7f
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2021-12-20 17:44:42 +0300

    Test home-page

---
+++ src/memo/tests/integration/test_views/test_home.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class HomePageTest(TestCase):
+    def test_response_from_home_page_view(self):
+        resp = self.client.get(reverse('memo:home'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html', 'base.html')
