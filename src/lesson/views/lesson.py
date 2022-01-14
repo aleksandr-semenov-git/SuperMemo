e83f4537ce39706d73e955086c6700b8cd91bcb4
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-01-14 14:34:47 +0300

    "Refactor edit-question-page
    delete edit_question service: useless"

--- src/lesson/views/lesson.py
+++ src/lesson/views/lesson.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from lesson.forms import LearningForm
+from lesson.forms import AddEditQuestionForm
 from lesson.models import Theme, Lesson, Question
 from memo.services import GoalService
 from lesson.services.model_service import SectionService, ThemeService, LessonService, QuestionService
@@ -21,7 +21,7 @@ class LessonLearnPage(View):
         name = f'{goal.name} {section_name} {theme_name}'
         lesson = LessonService.get_or_create_lesson(name=name, goal=goal, theme=theme)
 
-        form = LearningForm()
+        form = AddEditQuestionForm()
         return render(request, 'lesson_learn.html', {'form': form, 'lesson': lesson})
 
     def post(self, request, theme_id, *args, **kwargs):
@@ -29,7 +29,7 @@ class LessonLearnPage(View):
 
         Control active-lesson by django-sessions with theme_id, active_lesson_id keys
         """
-        form = LearningForm(request.POST)
+        form = AddEditQuestionForm(request.POST)
         theme = ThemeService.get_theme_by_id(theme_id)
         lesson = Lesson.objects.get(theme__id=theme.id)
         if form.is_valid():
@@ -48,19 +48,18 @@ class LessonLearnPage(View):
 @method_decorator(login_required, name='dispatch')
 class EditQuestionPage(View):
     def get(self, request, question_id, *args, **kwargs):
-        form = LearningForm()
         question = Question.objects.get(pk=question_id)
+        form = AddEditQuestionForm(initial={'question': question.question,
+                                            'answer': question.answer})
         return render(request, 'edit_question.html', {'form': form, 'question': question})
 
     def post(self, request, question_id):
-        form = LearningForm(request.POST)
+        question = Question.objects.get(pk=question_id)
+        form = AddEditQuestionForm(request.POST, instance=question)
         lesson = Question.objects.get(pk=question_id).lesson
         theme_id = lesson.theme.id
         if form.is_valid():
-            cd = form.cleaned_data
-            question = cd['question']
-            answer = cd['answer']
-            QuestionService.edit_question(question_id=question_id, question=question, answer=answer)
+            form.save(commit=True)
             return redirect('lesson:lesson_learn', theme_id=theme_id)
         else:
             return render(request, 'lesson_learn.html', {'form': form, 'lesson': lesson})
