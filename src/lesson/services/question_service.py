0dc26cdd6d08e6f4bf5d228c02540219ea1c4651
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-01-21 14:51:55 +0300

    "Add 2 services to the QuestionService"

--- src/lesson/services/question_service.py
+++ src/lesson/services/question_service.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from lesson.models import Lesson, Question
+from datetime import timedelta, datetime
 
 
 class QuestionService:
@@ -11,3 +12,16 @@ class QuestionService:
     @staticmethod
     def get_question_by_id(question_id: id) -> Question:
         return get_object_or_404(Question, pk=question_id)
+
+    @staticmethod
+    def cycle_to_days(self, cycle):
+        days_dict = {0: 0, 1: 1, 2: 2, 3: 12, 4: 20, 5: 30, 6: 60, 7: 90, 8: 150, 9: 270, 10: 480, 11: 720, 12: 1440,
+                     13: 2160, 14: 3960, 15: 6480}
+        days = days_dict[cycle]
+        return days
+
+    @staticmethod
+    def next_repeat_handler(self, cycle, prev_repeat_at):
+        days = QuestionService.cycle_to_days(self, cycle)
+        next_repeat_at = prev_repeat_at + timedelta(days=days)
+        return next_repeat_at
