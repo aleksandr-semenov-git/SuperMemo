6ed4b708023f1701fb815d04f7e228f5f9ec7620
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-01-31 11:05:12 +0300

    "Refactor repeat process
    refactor some html
    initialize admin models"

--- src/repeat/services/__init__.py
+++ src/repeat/services/__init__.py
@@ -1,1 +1,2 @@
-from repeat.services.repeat_session_service import RepeatSessionService
+from repeat.services.repeat_session_service import RepSessionService
+from repeat.services.qstate_service import QStateService
--- src/repeat/urls.py
+++ src/repeat/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import RepeatTheme, RepeatCheck, RepeatMix, RepeatSection, RepeatGoal
+from .views import RepeatTheme, Repeat, RepeatCheck, RepeatMix, RepeatSection, RepeatGoal
 
 urlpatterns = [
-    # path('learning-page/repeat/', RepeatMix.as_view(), name='lesson_repeat_mix'),
-    # path('learning-page/repeat/<int:goal_id>', RepeatGoal.as_view(), name='lesson_repeat_goal'),
-    # path('learning-page/repeat/<int:section_id>', RepeatSection.as_view(), name='lesson_repeat_section'),
-    path('learning-page/repeat/<int:theme_id>', RepeatTheme.as_view(), name='repeat_theme'),
+    path('learning-page/repeat/mix/', RepeatMix.as_view(), name='repeat_mix'),
+    path('learning-page/repeat/goal/<int:goal_id>', RepeatGoal.as_view(), name='repeat_goal'),
+    path('learning-page/repeat/section/<int:section_id>', RepeatSection.as_view(), name='repeat_section'),
+    path('learning-page/repeat/theme/<int:theme_id>', RepeatTheme.as_view(), name='repeat_theme'),
+
+    path('learning-page/repeat/<int:rep_id>', Repeat.as_view(), name='repeat'),
     path('learning-page/repeat/check/<int:question_id>', RepeatCheck.as_view(), name='repeat_check'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
