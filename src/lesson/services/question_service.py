0b06912b2fd5f36994fe9fa31e9b3fc73b90b4d6
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-02-01 14:19:24 +0300

    "Add docstring to the question services"

--- src/lesson/services/question_service.py
+++ src/lesson/services/question_service.py
@@ -14,21 +14,37 @@ from repeat.services import RepSessionService, QStateService
 class QuestionService:
     @staticmethod
     def create_question(question: Question, answer: str, lesson: Lesson) -> Question:
+        """Take question, answer, lesson, return question"""
         question = Question.objects.create(question=question, answer=answer, lesson=lesson)
         return question
 
     @staticmethod
     def get_question_by_id(question_id: id) -> Question:
+        """Take question_id, return question or raise Http404 exception"""
         return get_object_or_404(Question, pk=question_id)
 
     @staticmethod
-    def cycle_to_days(cycle):
+    def cycle_to_days(cycle: int) -> int:
+        """Take question's cycle, convert it to days with help of DAYS_DICT"""
         days_dict = settings.DAYS_DICT
         days = days_dict[cycle]
         return days
 
     @staticmethod
     def calculate_new_cycle(cycle: int, score: int) -> int:
+        """Calculate new question's cycle.
+
+        Take question's cycle and question's qstate-score. The logic is simple. If user repeated question less then 3
+        times, he will repeat this question at the next cycle. Elseif user repeated question 3 - 5 times,
+        the cycle of current question will be reduced. Elseif user repeated question more then 5 times, he will repeat
+        current question tomorrow. (rows 54-60)
+        For questions with cycle 1 was provided a bit different logic to not get negative cycle value. (rows 61-66)
+                Parameters:
+                        cycle (int): integer
+                        score (int): integer
+                Returns:
+                        new_cycle (int): integer
+        """
         new_cycle = cycle
         if cycle > 1:
             if score < 3:
@@ -46,13 +62,15 @@ class QuestionService:
         return new_cycle
 
     @staticmethod
-    def next_repeat_handler(cycle, prev_repeat_at):
+    def next_repeat_handler(cycle: int, prev_repeat_at: date):
+        """Calculate the day question should be repeated next time"""
         days = QuestionService.cycle_to_days(cycle)
         next_repeat_at = prev_repeat_at + timedelta(days=days)
         return next_repeat_at
 
     @staticmethod
     def get_today_questions_by_profile(profile: Profile) -> QuerySet:
+        """Get queryset of questions which should be repeated today"""
         today_date = date.today()
         QuestionService.renew_date_of_all_forgotten_questions(profile)
         questions = Question.objects.filter(Q(lesson__goal__profile=profile) &
@@ -61,6 +79,15 @@ class QuestionService:
 
     @staticmethod
     def get_next_question_by_rep_session(rep_session: RepetitionSession) -> Question:
+        """Get question from rep_session which should be repeated next.
+
+        Filter rep_session's questions by today's date, sort them by edit-time (which is important to always get next
+        question during repeat process) and then get question which was edited earlier.
+                Parameters:
+                        rep_session (RepetitionSession):
+                Returns:
+                        next_question (Question):
+        """
         today_date = date.today()
         next_question = \
             rep_session.questions.filter(next_repeat_at=today_date).order_by('edited_at').first()
@@ -68,6 +95,14 @@ class QuestionService:
 
     @staticmethod
     def renew_date_of_all_forgotten_questions(profile: Profile):
+        """Find not repeated questions and put their repeat date on today's date.
+
+        Return updated_questions_num as a control tool for developers.
+                Parameters:
+                        profile (Profile): user's profile
+                Returns:
+                        next_question (Question):
+        """
         today_date = date.today()
         yesterday_date = today_date - timedelta(days=1)
         forgotten_questions = Question.objects.filter(Q(lesson__goal__profile=profile) &
@@ -78,8 +113,19 @@ class QuestionService:
         return updated_questions_num
 
     @staticmethod
-    def save_remembered_perfectly(question: Question, profile: Profile):
-        rep_session, rep_mod = RepSessionService.get_or_create_rep_session_mix(profile=profile, questions=QuerySet())
+    def save_remembered_perfectly(question: Question, profile: Profile) -> int or Exception:
+        """Save question which was marked as remembered perfectly by the user.
+
+        Get rep_session and rep_mod (rep_mod is needed to control bugs).
+        Get score by question_id and rep_session_id, get current cycle, calculate new_cycle, collect statistics,
+        renew prev_repeat_at attribute for today's date, calculate next_repeat_at date and set it, save question.
+                Parameters:
+                        question (Question):
+                        profile (Profile): user's profile
+                Returns:
+                        rep_session_id (int): id of the current rep_session
+        """
+        rep_session, rep_mod = RepSessionService.get_or_create_rep_session_mix(profile=profile, questions=None)
         if rep_mod == 'active_rep_exists':
             score = QStateService.get_qstate_by_q_id_and_rep_id(question_id=question.id,
                                                                 rep_session_id=rep_session.id).score
@@ -103,8 +149,18 @@ class QuestionService:
             # Todo: Exception
 
     @staticmethod
-    def question_not_remembered(question, profile):
-        rep_session, rep_mod = RepSessionService.get_or_create_rep_session_mix(profile=profile, questions=QuerySet())
+    def question_not_remembered(question: Question, profile: Profile) -> int or Exception:
+        """Save question and it's score which if question was marked as not remembered clear by the user.
+
+        Get rep_session and rep_mod (rep_mod is needed to control bugs).
+        Get score by question_id and rep_session_id, change score to +1 value and save it (qstate.score). Save question.
+                Parameters:
+                        question (Question):
+                        profile (Profile): user's profile
+                Returns:
+                        rep_session_id (int): id of the current rep_session
+        """
+        rep_session, rep_mod = RepSessionService.get_or_create_rep_session_mix(profile=profile, questions=None)
         if rep_mod == 'active_rep_exists':
             qstate = QStateService.get_qstate_by_q_id_and_rep_id(question_id=question.id,
                                                                  rep_session_id=rep_session.id)
