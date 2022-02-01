49c47df14732c6732bbad3136febe90b77398c63
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-02-01 16:23:56 +0300

    "Add docstring to the qstate services"

--- src/repeat/services/qstate_service.py
+++ src/repeat/services/qstate_service.py
@@ -4,5 +4,6 @@ from repeat.models import QState
 class QStateService:
     @staticmethod
     def get_qstate_by_q_id_and_rep_id(question_id, rep_session_id: int) -> QState:
+        """Take question_id and rep_session_id, return QState object"""
         qstate = QState.objects.get(question__id=question_id, rep_session__id=rep_session_id)
         return qstate
