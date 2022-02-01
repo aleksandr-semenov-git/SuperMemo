542cddca53dfa8062f38035bf0004d53fdb9b7e9
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-02-01 17:57:25 +0300

    "Add docstring to the memo/views/goal.py"

--- src/memo/views/goal.py
+++ src/memo/views/goal.py
@@ -9,7 +9,7 @@ from memo.services import GoalService
 @method_decorator(login_required, name='dispatch')
 class MyGoalsPage(View):
     def get(self, request, *args, **kwargs):
-        """"""
+        """Render page. User see all he's goals"""
         goals = GoalService.get_goals_by_profile(profile=request.user.profile)
         return render(request, 'my_goals.html', {'goals': goals})
 
@@ -17,7 +17,7 @@ class MyGoalsPage(View):
 @method_decorator(login_required, name='dispatch')
 class GoalPage(View):
     def get(self, request, goal_id, *args, **kwargs):
-        """"""
+        """Render page. User see details of the goal"""
         goal = GoalService.get_goal_by_id(goal_id)
         request.session['goal_id'] = goal_id
         return render(request, 'goal_page.html', {'goal': goal})
@@ -26,12 +26,12 @@ class GoalPage(View):
 @method_decorator(login_required, name='dispatch')
 class AddGoalPage(View):
     def get(self, request, *args, **kwargs):
-        """"""
+        """Render page. User see AddGoalForm and fill it in"""
         form = AddGoalForm()
         return render(request, 'add_goal.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
-        """"""
+        """Redirect user to the my_goals page. Validate the form"""
         form = AddGoalForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
