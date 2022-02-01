7350d7bdb206f59357c845a4e550f96c5e9523ce
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-02-01 18:51:37 +0300

    "Add docstring to the lesson_service.py"

--- src/lesson/services/lesson_service.py
+++ src/lesson/services/lesson_service.py
@@ -8,30 +8,27 @@ from memo.models import Goal
 class LessonService:
     @staticmethod
     def get_lesson_by_id(lesson_id: int) -> Lesson:
+        """Take lesson_id, return Lesson"""
         return Lesson.objects.get(pk=lesson_id)
 
     @staticmethod
     def get_lessons_by_profile(profile: Profile) -> QuerySet:
+        """Take profile, return filtered queryset with all lesson which have goals which are connected to the profile"""
         return Lesson.objects.filter(goal__profile=profile)
 
     @staticmethod
     def get_lesson_by_theme_id(theme_id: int) -> Lesson:
+        """Take theme_id, return lesson which theme have theme_id"""
         return Lesson.objects.get(theme__id=theme_id)
 
     @staticmethod
     def get_or_create_lesson(goal: Goal, theme: Theme) -> Lesson:
+        """Take goal and theme, return existing or new lesson.
+
+        Build lesson's name from theme_name, section_name and goal_name, then use get_or_create method.
+        """
         theme_name = theme.name
         section_name = theme.section.name
         name = f'{goal.name} {section_name} {theme_name}'
         lesson, created = Lesson.objects.get_or_create(name=name, goal=goal, theme=theme)
         return lesson
-
-    @staticmethod
-    def check_repeat_lesson(theme_id: int, checked_questions: list) -> bool:
-        questions_num = Theme.objects.get(pk=theme_id).lesson.questions.count()
-        if len(checked_questions) < questions_num:
-            return False
-        elif len(checked_questions) == questions_num:
-            return True
-        else:
-            pass  # Todo: raise error
