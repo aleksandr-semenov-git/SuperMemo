f5ef703be076189c2b24b862386528abe2538aa6
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-02-02 13:20:51 +0300

    "Delete ChooseSectionPage and ChooseThemePage, delete urls"

--- src/lesson/views/section.py
+++ src/lesson/views/section.py
@@ -1,29 +1,11 @@
-from django.shortcuts import render, redirect
-from django.views import View
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.utils.decorators import method_decorator
-from lesson.forms import ChooseSectionForm, AddSectionForm
-from memo.services import GoalService
+from django.views import View
+
+from lesson.forms import AddSectionForm
 from lesson.services import SectionService
-
-
-@method_decorator(login_required, name='dispatch')
-class ChooseSectionPage(View):
-    def get(self, request, *args, **kwargs):
-        goal = GoalService.get_goal_by_id(request.session['goal_id'])
-        form = ChooseSectionForm(goal_id=request.session['goal_id'])
-        return render(request, 'choose_section.html', {'form': form, 'goal': goal})
-
-    def post(self, request, *args, **kwargs):
-        form = ChooseSectionForm(request.POST, goal_id=request.session['goal_id'])
-        if form.is_valid():
-            cd = form.cleaned_data
-            section = SectionService.get_section_by_name_and_goal_id(name=cd['name'],
-                                                                     goal_id=request.session['goal_id'])
-            return redirect('lesson:choose_theme', section_id=section.id)
-        else:
-            goal = GoalService.get_goal_by_id(request.session['goal_id'])
-            return render(request, 'choose_section.html', {'form': form, 'goal': goal})
+from memo.services import GoalService
 
 
 @method_decorator(login_required, name='dispatch')
