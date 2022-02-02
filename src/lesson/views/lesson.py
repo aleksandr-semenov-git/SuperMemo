a5c625cfd003cac6c372ff55265b5eeb5953905c
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-02-02 13:13:48 +0300

    "Add docstring to the lesson.py"

--- src/lesson/views/lesson.py
+++ src/lesson/views/lesson.py
@@ -11,21 +11,23 @@ from memo.services import GoalService
 @method_decorator(login_required, name='dispatch')
 class LessonLearnPage(View):
     def get(self, request, theme_id, *args, **kwargs):
-        """Render lesson-page and LearningForm"""
-        request.session['theme_id'] = theme_id
+        """Render page. User see LearningForm and table with questions and answers.
+
+        User can edit or delete questions. User see logout, repeat-theme, back-to-<goal>, back-to-profile,
+        back-to-my-goals buttons.
+        """
         goal = GoalService.get_goal_by_id(request.session['goal_id'])
         theme = ThemeService.get_theme_by_id(theme_id)
         lesson = LessonService.get_or_create_lesson(goal=goal, theme=theme)
-        request.session[f'lesson{lesson.id}'] = []
-
         form = AddEditQuestionForm()
         return render(request, 'lesson_learn.html', {'form': form, 'lesson': lesson})
 
     def post(self, request, theme_id, *args, **kwargs):
-        """Check data from learning-form. Render lesson-page again"""
+        """Check data from LearningForm. Render lesson-page again. Show form errors if needed"""
         form = AddEditQuestionForm(request.POST)
         theme = ThemeService.get_theme_by_id(theme_id)
-        lesson = LessonService.get_lesson_by_theme_id(theme.id)
+        theme_id = theme.id
+        lesson = LessonService.get_lesson_by_theme_id(theme_id)
         if form.is_valid():
             cd = form.cleaned_data
             question = cd['question']
@@ -41,6 +43,7 @@ class LessonLearnPage(View):
 
 @method_decorator(login_required, name='dispatch')
 class EditQuestionPage(View):
+    """Render page. User see question, answer, AddEditQuestionForm, logout and save-question buttons"""
     def get(self, request, question_id, *args, **kwargs):
         question = QuestionService.get_question_by_id(question_id)
         form = AddEditQuestionForm(initial={'question': question.question,
@@ -48,9 +51,10 @@ class EditQuestionPage(View):
         return render(request, 'edit_question.html', {'form': form, 'question': question})
 
     def post(self, request, question_id):
+        """Check data from AddEditQuestionForm. Render lesson-page. Show form errors if needed."""
         question = QuestionService.get_question_by_id(question_id)
         form = AddEditQuestionForm(request.POST, instance=question)
-        lesson = QuestionService.get_question_by_id(question_id).lesson
+        lesson = question.lesson
         theme_id = lesson.theme.id
         if form.is_valid():
             form.save(commit=True)
@@ -62,8 +66,9 @@ class EditQuestionPage(View):
 @method_decorator(login_required, name='dispatch')
 class DeleteQuestionView(View):
     def get(self, request, question_id, *args, **kwargs):
+        """Delete chosen Question object by id. Render lesson-page again"""
         question = QuestionService.get_question_by_id(question_id)
-        lesson = QuestionService.get_question_by_id(question_id).lesson
+        lesson = question.lesson
         theme_id = lesson.theme.id
         question.delete()
         return redirect('lesson:lesson_learn', theme_id=theme_id)
