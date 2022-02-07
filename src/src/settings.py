196af31dbf1deaa8eb729e3cb5288907cbb0259b
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-02-07 14:51:54 +0300

    "Edit settings.py"

--- src/src/settings.py
+++ src/src/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates', 'account/templates', 'memo/templates', 'lesson/templates'],
+        'DIRS': ['templates', 'account/templates', 'memo/templates', 'lesson/templates', 'repeat/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
