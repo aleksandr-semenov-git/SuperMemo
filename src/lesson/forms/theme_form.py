9230e8d6b45ff2349621ee6bb6d7c572718c2c2a
alexander-semenov-coherent-lab <AlexanderSemenov@coherentsolutions.com>
2022-02-17 19:29:53 +0300

    "Delete ChooseThemeForm"

--- src/lesson/forms/theme_form.py
+++ src/lesson/forms/theme_form.py
@@ -16,27 +16,3 @@ class AddThemeForm(forms.ModelForm):
     class Meta:
         model = Theme
         fields = ['name']
-
-
-class ChooseThemeForm(forms.Form):
-    """
-    This is forms.Form class. Class which construct dropdown menu where user will chose one theme
-
-    Class attributes
-    ----------------
-    name : ModelChoiceField
-
-    Methods
-    -------
-    __init__(self, *args, **kwargs)
-        this method takes parameter section_id from the kwargs and provide required queryset as choices
-    """
-    name = forms.ModelChoiceField(queryset=Theme.objects.none(),
-                                  empty_label='Choose theme')
-
-    def __init__(self, *args, **kwargs):
-        section_id = kwargs.pop('section_id', None)
-        super().__init__(*args, **kwargs)
-
-        if section_id:
-            self.fields['name'].queryset = Theme.objects.filter(section__id=section_id)
