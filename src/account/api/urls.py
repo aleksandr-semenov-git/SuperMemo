ef7fd3db7f1891d283f570e9c1b8834fb1c5acc2
JianYang <jian9ang-git@ya.ru>
2022-02-22 12:37:34 +0300

    Add djoser urls

--- src/account/api/urls.py
+++ src/account/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.conf import settings
 from .api_views import ProfileDetails
+from django.conf.urls import include
 
 urlpatterns = [
-    path('profile/<int:pk>/', ProfileDetails.as_view(), name='profile_details')
+    path('profile/<int:pk>/', ProfileDetails.as_view(), name='profile_details'),
 ]
