fa06772035f7bfa0d63eae56d1d62792cf52ff7b
aleksandr-semenov-git <jian9ang@ya.ru>
2022-03-06 18:59:11 +0300

    "Add user/profile/id url"

--- src/account/api/urls.py
+++ src/account/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf import settings
-from .api_views import ProfileDetails
+from .api_views import ProfileDetails, UserProfileDetails
 from django.conf.urls import include
 
 urlpatterns = [
+    path('user/profile/<int:pk>/', UserProfileDetails.as_view(), name='user_profile_details'),
     path('profile/<int:pk>/', ProfileDetails.as_view(), name='profile_details'),
 ]
