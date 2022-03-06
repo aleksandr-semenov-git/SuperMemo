1de02a14f710d7611cc34f581e17c46b5fb199c5
aleksandr-semenov-git <jian9ang@ya.ru>
2022-03-06 19:01:40 +0300

    "Refactor, add urls
    refactor path of rest_framework.urls
    refactor path of djoser.urls
    add api/auth-token/ path of djoser.urls.authtoken
    "

--- src/src/urls.py
+++ src/src/urls.py
@@ -22,8 +22,9 @@ urlpatterns = [
     path('api/', include(('lesson.api.urls', 'lesson_api'), namespace='lesson_api')),
     path('api/', include(router.urls)),
     path('__debug__/', include('debug_toolbar.urls')),
-    path('auth/', include('djoser.urls')), 
-    path('auth_token/', include('djoser.urls.auth_token')), 
+    path('api/base-auth/', include('rest_framework.urls')),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth-token/', include('djoser.urls.authtoken')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
