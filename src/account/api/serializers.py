ecbe61715c5a80c696b220a4cf20ac7abd868764
aleksandr-semenov-git <jian9ang@ya.ru>
2022-03-06 18:56:04 +0300

    "Add ProfileDetailsSerializer
    rename ProfileDetailsSerializer to UserProfileDetailsSerializer"

--- src/account/api/serializers.py
+++ src/account/api/serializers.py
@@ -5,7 +5,15 @@ from django.contrib.auth.models import User
 from memo.models import Goal
 
 
-class ProfileDetailsSerializer(serializers.ModelSerializer):
+class UserProfileDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+
+class ProfileDetailsSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Profile
         fields = '__all__'
