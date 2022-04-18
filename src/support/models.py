7ad41c0b4ccca1f506a4a01d57a7915b862020ed
aleksandr-semenov-git <jian9ang@ya.ru>
2022-04-18 16:00:07 +0300

    "Create basic Ticket and Message models"

--- src/support/models.py
+++ src/support/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+IN_PROGRESS = 'INPR'
+CLOSED = 'CLSD'
+FREEZE = 'FRZ'
+STATUS_CHOICES = (
+    (IN_PROGRESS, 'in_progress'),
+    (CLOSED, 'closed'),
+    (FREEZE, 'freeze'),
+)
+
+
+class Ticket(models.Model):
+    users = models.ManyToManyField(User, related_name='tickets')
+    status = models.CharField(choices=STATUS_CHOICES, max_length=11, default=IN_PROGRESS)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+    # frozen_at = models.DateField(default=, null=True)
+    # freeze_issue = models.CharField(choices=FREEZE_CHOICES)
+    # closed_at = models.DateField(default=, null=True)
+
+
+class Message(models.Model):
+    text = models.TextField(null=False, blank=False)
+    user = models.ForeignKey(User, related_name='messages', verbose_name='user', on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, related_name='messages', verbose_name='ticket', on_delete=models.CASCADE)
+
