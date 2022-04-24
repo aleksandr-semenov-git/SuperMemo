387a91281c77810ebabd6c0eef45066982fb547a
aleksandr-semenov-git <jian9ang@ya.ru>
2022-04-24 15:05:12 +0300

    "Add MessageInline and TicketAdmin models"

--- src/support/admin.py
+++ src/support/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from support.models import Ticket, Message
 
-# Register your models here.
+
+class MessageInline(admin.StackedInline):
+    model = Message
+    extra = 1
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('status', 'issue', 'description')
+    list_filter = ('status',)
+    search_fields = ('created_at', 'edited_at', )
+    inlines = [MessageInline]
+
+
+admin.site.register(Message)
