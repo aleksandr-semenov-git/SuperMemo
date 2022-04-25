f8f5024ebba795d2101e328c6ec2e465258d5ac1
aleksandr-semenov-git <jian9ang@ya.ru>
2022-04-25 19:56:23 +0300

    "Refactor urls.py. Add format_suffix_patterns"

--- src/support/api/urls.py
+++ src/support/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from support.api.api_views.ticket_views import TicketViewSet
 
-urlpatterns = [
-    path('user/get/tickets/', TicketViewSet.as_view({'get': 'list'})),
-]
+tickets_list = TicketViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
+urlpatterns = format_suffix_patterns([
+    path('user/tickets/', tickets_list, name='user_tickets'),
+])
