621031290f6841a011b76a12854e5717beaa36ff
aleksandr-semenov-git <jian9ang@ya.ru>
2022-04-26 19:07:47 +0300

    "Add message_list urls"

--- src/support/api/urls.py
+++ src/support/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from support.api.api_views.message_views import MessageViewSet
 from support.api.api_views.ticket_views import TicketViewSet
 
 tickets_list = TicketViewSet.as_view({
@@ -8,6 +9,12 @@ tickets_list = TicketViewSet.as_view({
     'post': 'create',
 })
 
+message_list = MessageViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
 urlpatterns = format_suffix_patterns([
     path('user/tickets/', tickets_list, name='user_tickets'),
+    path('user/ticket/<int:ticket_id>/messages/', message_list, name='user_messages'),
 ])
