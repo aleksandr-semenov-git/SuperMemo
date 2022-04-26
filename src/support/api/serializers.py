f67cf5b0b3c74f4a66f8faf736b1db3de07adad8
aleksandr-semenov-git <jian9ang@ya.ru>
2022-04-26 15:22:28 +0300

    "Redefine create method in TicketSerializer
    When user create ticket `.create()` use `.find_support()` service to get support-user."

--- src/support/api/serializers.py
+++ src/support/api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from support.models import Ticket
@@ -7,3 +8,14 @@ class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = '__all__'
+
+    def create(self, validated_data):
+        users = validated_data.get("users", None)
+        validated_data.pop("users")
+        # We get JSON where field `users` contain only 1 user
+        user = users[0]
+        support = SupportService.find_support()
+        new_ticket = Ticket.objects.create(**validated_data)
+        new_ticket.users.set((user.id, support.id))
+        return new_ticket
+
