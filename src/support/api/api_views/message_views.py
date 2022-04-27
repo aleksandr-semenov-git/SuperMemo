d1e91cd8075555d8cc6f7cba27f48bc174ec9884
aleksandr-semenov-git <jian9ang@ya.ru>
2022-04-28 00:02:57 +0300

    "Refactor MessageViewSet create method. Add get_permissions method"

--- src/support/api/api_views/message_views.py
+++ src/support/api/api_views/message_views.py
@@ -1,8 +1,13 @@
+from django.http import HttpResponseRedirect
 from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
+from support.api.permissions import IsThreadParticipant
 from support.api.serializers import MessageSerializer
 from support.models import Message
+from support.services import TicketService
 
 
 class MessageViewSet(viewsets.ViewSet):
@@ -13,11 +18,18 @@ class MessageViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request, ticket_id):
-        serializer = MessageSerializer(data=request.data)
+        user_id = request.user.id
+        ticket_id = TicketService.get_ticket_by_id(ticket_id).id
+        serializer = MessageSerializer(data=request.data, context={'user_id': user_id, 'ticket_id': ticket_id})
         if serializer.is_valid():
             new_message = serializer.save()
             ticket_id = new_message.ticket.id
-            queryset = Message.objects.filter(ticket__id=ticket_id)
-            serializer = MessageSerializer(queryset, many=True)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return HttpResponseRedirect(reverse('support_api:user_messages', kwargs={'ticket_id': ticket_id}))
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_permissions(self):
+        if self.action in ('list', 'create'):
+            permission_classes = [IsAuthenticated, IsThreadParticipant]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
