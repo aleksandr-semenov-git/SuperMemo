140e5bb090dced9b10c5facb30a91ac8b861c27e
aleksandr-semenov-git <jian9ang@ya.ru>
2022-04-28 19:16:08 +0300

    "Redefined save_model method in TicketAdmin"

--- src/support/admin.py
+++ src/support/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from support.models import Ticket, Message
+from django.core.mail import send_mail
 
 
 class MessageInline(admin.TabularInline):
@@ -22,6 +23,23 @@ class TicketAdmin(admin.ModelAdmin):
             "fields": ('description',)
         })
     )
+
+    def save_model(self, request, obj, form, change):
+        data = form.changed_data
+        user = obj.users.filter(is_staff=False).first()
+        username, email = user.username, user.email
+        ticket_id = obj.id
+        support = request.user.username
+        if 'status' in data:
+            status = data['status']
+            send_mail(subject=f'Status of your ticket №{ticket_id} was changed',
+                      message=f'Hello, dear {username}.'
+                              f'We want you to know that {support} have changed status of your ticket to {status}.'
+                              f'Best regards, CEO of GlobeMemo inc. Aleksandr Semenov',
+                      from_email=None,
+                      recipient_list=[email])
+
+        super().save_model(self, request, obj, form)
 
 
 admin.site.register(Message)
