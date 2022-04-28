2d476c4bc76679feebac63f42c4f2f6b7378a1aa
aleksandr-semenov-git <jian9ang@ya.ru>
2022-04-28 20:57:15 +0300

    "Refactor list_display, list_filter in TicketAdmin"

--- src/support/admin.py
+++ src/support/admin.py
@@ -10,8 +10,8 @@ class MessageInline(admin.TabularInline):
 
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
-    list_display = ('status', 'issue', 'description')
-    list_filter = ('status',)
+    list_display = ('status', 'issue', 'description', 'support')
+    list_filter = ('status', 'support__groups')
     search_fields = ('created_at', 'edited_at', )
     inlines = [MessageInline]
     save_on_top = True
