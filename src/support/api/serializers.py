353e9a7e61a4ef91988214a185843fe03ebe82ed
aleksandr-semenov-git <jian9ang@ya.ru>
2022-04-28 12:08:20 +0300

    "Add docstring and type annotation to MessageSerializer and TicketSerializer methods"

--- src/support/api/serializers.py
+++ src/support/api/serializers.py
@@ -9,7 +9,8 @@ class TicketSerializer(serializers.ModelSerializer):
         model = Ticket
         fields = '__all__'
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Ticket:
+        """Redefine create method to automatically find the most free support and add him to the ticket"""
         support = TicketService.find_support()
         new_ticket = TicketService.create_ticket(validated_data)
         new_ticket.users.set(support.id)
@@ -21,7 +22,11 @@ class MessageSerializer(serializers.ModelSerializer):
         model = Message
         fields = '__all__'
 
-    def validate(self, data):
+    def validate(self, data: dict) -> dict:
+        """
+        Compare POST-request-data with user's data to check if user can leave messages in the ticket.
+        It's also prevent user to somehow leave messages in a wrong ticket.
+        """
         context = self.context
         if context['ticket_id'] != data['ticket'].id or context['user_id'] != data['user'].id:
             raise serializers.ValidationError('Only participant of thread can create a message on this page.')
