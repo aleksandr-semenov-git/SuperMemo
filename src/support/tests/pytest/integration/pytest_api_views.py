c1a76c6de755e7162a3629a2f8c98b9a1bf512b0
aleksandr-semenov-git <jian9ang@ya.ru>
2022-05-02 23:22:51 +0300

    "Add first integration pytest - test_tickets_list"

---
+++ src/support/tests/pytest/integration/pytest_api_views.py
@@ -0,0 +1,17 @@
+import pytest
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@pytest.mark.django_db
+def test_tickets_list(user, client, ticket):
+    login = client.post(reverse('account:login'), data={'username': user.username, 'password': '121212ab'})
+    result = client.get(reverse('support_api:user_tickets'))
+    data = dict(result.data[0])
+
+    assert result.status_code, 200
+    assert isinstance(result, Response)
+    assert data['id'] == ticket.id
+    assert data['description'] == ticket.description
+    assert data['user'] == ticket.user.id
+    assert data['support'] == ticket.support.id
