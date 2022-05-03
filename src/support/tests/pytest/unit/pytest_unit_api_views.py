34e8798dbda43b935c0198d1070398550f993dea
aleksandr-semenov-git <jian9ang@ya.ru>
2022-05-03 21:37:13 +0300

    "Add MockTicketSerializer and mock_new method"

--- src/support/tests/pytest/unit/pytest_unit_api_views.py
+++ src/support/tests/pytest/unit/pytest_unit_api_views.py
@@ -2,6 +2,7 @@ import functools
 from unittest.mock import MagicMock
 
 from support.api.api_views import TicketViewSet
+from support.api.serializers import TicketSerializer
 from support.services import TicketService
 
 
@@ -22,16 +23,30 @@ class MockTicketServices:
         return [mock_ticket1, mock_ticket2]
 
 
+class MockTicketSerializer:
+    @staticmethod
+    def new():
+        test_data = {'id': 1, 'user': 1, 'support': 1}
+        return MagicMock(data=test_data)
+
+
 def test_tickets_list(monkeypatch):
     @trackcalls
     def mock_filter(*args, **kwargs):
         return MockTicketServices.filter_tickets_by_user_id()
 
-    monkeypatch.setattr(TicketService, 'filter_tickets_by_user_id', mock_filter)
-    mock_request = MagicMock(user=MagicMock(id=11))
+    @trackcalls
+    def mock_new(*args, **kwargs):
+        return MockTicketSerializer.new()
 
+    monkeypatch.setattr(TicketService, 'filter_tickets_by_user_id', mock_filter)
+    monkeypatch.setattr(TicketSerializer, '__new__', mock_new)
+
+    mock_request = MagicMock(user=MagicMock(id=11))
     view = TicketViewSet(request=mock_request)
     result = view.list(mock_request)
 
     assert mock_filter.has_been_called
+    assert mock_new.has_been_called
+
 
